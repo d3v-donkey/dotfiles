b4064754648ad1161e7dbd59df096ce027c9db6c
d3v-donkey <alexandre_maury@outlook.com>
2019-09-08 18:45:28 +0200

    modification de lamp

--- bin/lamp.sh
+++ bin/lamp.sh
@@ -13,47 +13,86 @@ install() {
 	sudo apt-get update 
 	sudo apt-get upgrade -y
 
-	echo "Donnez un Password 'fort' à votre compte root MySql :"
-	read mysqlrootpassword
+	################# database #################################################
+	echo "Votre choix : MySql (s) ou MariaDB (m) :"
+	read myDatabase
 
-	## install Apache 
+	if [ myDatabase == 's' ]; then
+		## install mysql
+		wget http://repo.mysql.com/mysql-apt-config_0.8.13-1_all.deb
+		sudo dpkg -i mysql-apt-config_0.8.13-1_all.deb
+		sudo apt update -y
+		sudo apt install mysql-server -y
+
+	elif [ myDatabase == 'm' ]; then
+		echo "Donnez un Password 'fort' à votre compte root MariaDB :"
+		read mysqlrootpassword
+
+		sudo apt install mariadb-server mariadb-client
+
+
+
+	else
+		echo 'Aucun gestionnaire de base de donnée choisi !'
+
+	fi
+
+	# config server sql
+	sudo mysql -e "UPDATE mysql.user SET authentication_string = PASSWORD('toor'), plugin = 'mysql_native_password' WHERE User = 'root' AND Host = 'localhost';"
+	sudo mysql -e "DROP USER 'phpmyadmin'@'localhost'"
+	sudo mysql -e "DROP DATABASE phpmyadmin"
+	sudo mysql -e "FLUSH PRIVILEGES"
+
+	################# Apache #################################################
 	sudo apt install apache2 apache2-doc -y
 
 
-	## install php
+	################# PHP ####################################################
 	sudo apt install php libapache2-mod-php php-mysql php-curl php-gd \
 	php-intl php-json php-mbstring php-xml php-zip php-gettext -y
 
 
-	## install mysql
-	wget http://repo.mysql.com/mysql-apt-config_0.8.13-1_all.deb
-	sudo dpkg -i mysql-apt-config_0.8.13-1_all.deb
-	sudo apt update -y
-	sudo apt install mysql-server -y
+	################# phpmyadmin + configuration d'apache pour phpmyadmin ###
+	wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.zip 
+	sudo unzip phpMyAdmin-4.9.0.1-all-languages.zip -d /opt
+	sudo mv -v /opt/phpMyAdmin-4.9.0.1-all-languages /opt/phpMyAdmin
+	sudo chown -Rfv www-data:www-data /opt/phpMyAdmin
 
-	## install phpmysql
-	sudo apt install phpmyadmin -y
+	echo "
+	<VirtualHost *:9000>
+	ServerAdmin webmaster@localhost
+	DocumentRoot /opt/phpMyAdmin
+	 
+	<Directory /opt/phpMyAdmin>
+	Options Indexes FollowSymLinks
+	AllowOverride none
+	Require all granted
+	</Directory>
+	ErrorLog ${APACHE_LOG_DIR}/error_phpmyadmin.log
+	CustomLog ${APACHE_LOG_DIR}/access_phpmyadmin.log combined
+	</VirtualHost>
+	" | sudo tee -a /etc/apache2/sites-available/phpmyadmin.conf > /dev/null
 
 
-	#config server sql
-	#sudo mysql -e "UPDATE mysql.user SET authentication_string = PASSWORD('$mysqlrootpassword'), plugin = 'mysql_native_password' WHERE User = 'root' AND Host = 'localhost';"
-	#sudo mysql -e "DROP USER 'phpmyadmin'@'localhost'"
-	#sudo mysql -e "DROP DATABASE phpmyadmin"
-	#sudo mysql -e "FLUSH PRIVILEGES"
+	echo "
+	Listen 9000
+	" | sudo tee -a /etc/apache2/ports.conf > /dev/null
+
 
 	# S'ajouter au groupe www-data
-	sudo usermod -a -G www-data $(whoami)
+	# sudo usermod -a -G www-data $(whoami)
 
 	# Associer le groupe www-data au dossier /var/www
-	sudo chgrp -R www-data /var/www/
+	# sudo chgrp -R www-data /var/www/
 
 	# Donne les droits d'écriture a l'utilisateur
-	sudo chown $(whoami):www-data /var/www -R
-	#sudo chmod -R g+rwxs /var/www/
+	# sudo chown $(whoami):www-data /var/www -R
+	# sudo chmod -R g+rwxs /var/www/
 
 	# Créer un lien s'imbolique
-	sudo ln -s /var/www ~/www  > /dev/null
+	# sudo ln -s /var/www ~/www  > /dev/null
 
+	sudo a2ensite phpmyadmin.conf
 	sudo systemctl restart apache2
 	sudo systemctl restart mysql
 

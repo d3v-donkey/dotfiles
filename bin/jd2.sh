4ca7d0177f4efff165296393c6813f14ff23b987
Alexandre Maury <alexandre_maury@outlook.com>
2019-09-10 16:59:16 +0200

    Update jd2.sh

--- bin/jd2.sh
+++ bin/jd2.sh
@@ -3,5 +3,5 @@
 ### Jdownloader2 ###
 
 sudo mkdir /opt/JDownloader2
-cd /opt/JDownloader2 && git clone https://github.com/d3v-donkey/jdownloader.git
+cd /opt/JDownloader2 && sudo git clone https://github.com/d3v-donkey/jdownloader.git
 cd jdownloader && sudo chmod +x JD2Setup_x64.sh && ./JD2Setup_x64.sh

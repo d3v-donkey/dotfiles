b34a9e4825dbe866085beef76bcbf7c5591660e2
d3v-donkey <alexandre_maury@outlook.com>
2019-09-15 16:18:37 +0200

    modification du script

--- bin/lamp-start.sh
+++ bin/lamp-start.sh
@@ -8,28 +8,27 @@ lamp_start() {
 	MariaDB=$(dpkg -l | egrep "mariadb-server" 2> /dev/null || echo '')
 
 	if [ -n "$MySql" ]; then
-	    #sudo systemctl start mysql
-		echo "test mysql"
+	    echo $password | sudo -S systemctl start mysql
+
 	    
 	elif [ -n "$MariaDB" ]; then
-	    #sudo systemctl start mariadb
-		echo "test maria"
+	    echo $password | sudo -S systemctl start mariadb
+		
 	    
 	else
-
-		echo $password | sudo -S apt update
+	    echo "Aucune database installer"; notify-send "Start Sql Failed"
 	   
 	fi
 
 	Apache2=$(dpkg -l | egrep "apache2" 2> /dev/null || echo '')
 
 	if [ -n "$Apache2" ]; then
-	    #sudo systemctl start apache2
-		echo "test apache2"
+	    echo $password | sudo -S systemctl start apache2
+	
 	    
 	else
-	    #echo "pas de serveur"
-		echo "test not"
+	    echo "Aucun Server installer"; notify-send "Start Server Failed"
+		
 	   
 	fi
 }
@@ -41,23 +40,51 @@ lamp_stop() {
 	MariaDB=$(dpkg -l | egrep "mariadb-server" 2> /dev/null || echo '')
 
 	if [ -n "$MySql" ]; then
-	    sudo systemctl stop mysql
+	    echo $password | sudo -S systemctl stop mysql
 	    
 	elif [ -n "$MariaDB" ]; then
-	    sudo systemctl stop mariadb
+	    echo $password | sudo -S systemctl stop mariadb
 	    
 	else
-	    echo "pas de database"
+	    echo "Erreur"; notify-send "Stop Sql Failed"
 	   
 	fi
 
 	Apache2=$(dpkg -l | egrep "apache2" 2> /dev/null || echo '')
 
 	if [ -n "$Apache2" ]; then
-	    sudo systemctl stop apache2
+	    echo $password | sudo -S systemctl stop apache2
 	    
 	else
-	    echo "pas de serveur"
+	    echo "Aucun Server installer"; notify-send "Stop Server Failed"
+	   
+	fi
+}
+
+################# RESTART LAMP ######################################
+lamp_restart() {
+
+	MySql=$(dpkg -l | egrep "mysql-server" 2> /dev/null || echo '')
+	MariaDB=$(dpkg -l | egrep "mariadb-server" 2> /dev/null || echo '')
+
+	if [ -n "$MySql" ]; then
+	    echo $password | sudo -S systemctl restart mysql
+	    
+	elif [ -n "$MariaDB" ]; then
+	    echo $password | sudo -S systemctl restart mariadb
+	    
+	else
+	    echo "Erreur"; notify-send "Restart Sql Failed"
+	   
+	fi
+
+	Apache2=$(dpkg -l | egrep "apache2" 2> /dev/null || echo '')
+
+	if [ -n "$Apache2" ]; then
+	    echo $password | sudo -S systemctl restart apache2
+	    
+	else
+	    echo "Aucun Server installer"; notify-send "Restart Server Failed"
 	   
 	fi
 }
@@ -75,5 +102,23 @@ fi
 
 ####################### MAIN ###################################
 
-zenity --list   --title="Choose script" --column="Script" --column="Description" "a.sh" "chaise longue" "b.sh" "moineau"
+choix=$(zenity  --list   --title=" " --column="Lamp" --column="Description" "Start" "Lance les services Apache2 - MySQl ou MariaDB" "Stop" "Stop les services" "Status" "Apercut des services" "Restart" "Redémare les services" --width=550 --height=250)
+
+if [ $choix == "Start" ]; then
+	lamp_start
+
+elif [ $choix == "Stop" ]; then
+	lamp_stop
+
+elif [ $choix == "Status" ]; then
+	lamp_status
+
+elif [ $choix == "Restart" ]; then
+	lamp_restart
+else
+	echo "Choix non valide"
+
+fi
+
+
 

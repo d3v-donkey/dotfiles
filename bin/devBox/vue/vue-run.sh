68c3b0c6ff94c29913f6b6fdbbf16ebf5f031eeb
d3v-donkey <alexandre_maury@outlook.com>
2019-09-18 13:33:47 +0200

    d3v-donkey

--- bin/devBox/vue/vue-run.sh
+++ bin/devBox/vue/vue-run.sh
@@ -13,3 +13,20 @@ run_projet() {
     sudo npm run dev
 }
 
+
+########################################################################
+case "$1" in
+    --new-projet)
+        new_projet
+        ;;
+    --run-projet)
+        run_projet
+        ;;
+    --help)
+        help_
+        ;;
+
+    *) echo "[ Oups saisir sur votre terminal ./devBox --help ]"; 
+esac
+########################################################################
+

6b0acf9c8d53bfeab23d6ca81b0bee6a3ca364a8
d3v-donkey <alexandre_maury@outlook.com>
2019-09-19 13:54:32 +0200

    d3v-donkey

--- bin/devBox/react/reactJS-run.sh
+++ bin/devBox/react/reactJS-run.sh
@@ -4,12 +4,11 @@ new_projet() {
     echo "Veuillez Choisir le nom de votre projet"
     read name
 
-    cd ~/www/html && sudo ionic start $name
-    cd $name && sudo ionic cordova platform add android
+    cd ~/www/html && sudo create-react-app $name
 }
 
 run_projet() {
-    sudo ionic serve
+    sudo npm start
 }
 
 

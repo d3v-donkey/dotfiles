864dd656ae15e185cc7d0f89dc67a3fc339ea682
Alexandre Maury <alexandre_maury@outlook.com>
2019-09-20 15:12:00 +0200

    Add files via upload

---
+++ bin/devBox/alias.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+echo "
+ALIAS :                                           GIT :
+---------------------------------------           ---------------------------------------------------------------------
+[ youtube-dl ] => mp3                             > git config --global user.name "d3v-donkey" 
+---------------------------------------           > git config --global user.email "d3v-donkey@outlook.com" 
+[ wordpress and database ] => wordpress           ---------------------------------------------------------------------
+---------------------------------------           > git init
+[ symfony project Web ] => sy-web                 ---------------------------------------------------------------------
+---------------------------------------           > git pull https://github.com/username/projet_name.git
+[ symfony project Api ] => sy-api                 ---------------------------------------------------------------------
+---------------------------------------           > git add nouveau_fichier
+[ Run symfony project ] => symfony server:start   > git commit -m "d3v-donkey"
+---------------------------------------           > git remote add origin https://github.com/d3v-donkey/d3v-donkey.git                                                                
+[ Vue project new ] => vu-web                     > git remote -v                                                      
+---------------------------------------           > git push --set-upstream origin master                              
+[ Vue run project ] => npm run dev                ---------------------------------------------------------------------
+--------------------------------------- 
+[ ReactJs new project ] => re-web 
+--------------------------------------- 
+[ ReactJs run project ] => npm start 
+---------------------------------------
+[ ReactNat new project ] => rna-web 
+--------------------------------------- 
+[ ReactNat run project ] => npm start
+---------------------------------------  
+"
+

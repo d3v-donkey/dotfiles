62520b9f3a1f152f8720164014f29fe72a93ea2c
Alexandre Maury <alexandre_maury@outlook.com>
2019-09-26 12:40:54 +0200

    Update devBox.sh

--- bin/devBox/devBox.sh
+++ bin/devBox/devBox.sh
@@ -161,7 +161,52 @@ devBox() {
 
 		################# Apache #################################################
 		sudo apt install apache2 apache2-doc -y
+		
+		
+		echo '
+		NameVirtualHost *
+		<VirtualHost *>
+			ServerAdmin webmaster@localhost
 
+			DocumentRoot /var/www/
+			<Directory />
+				Options FollowSymLinks
+				AllowOverride None
+			</Directory>
+			<Directory /var/www/>
+				Options Indexes FollowSymLinks MultiViews
+				AllowOverride None
+				Order allow,deny
+				allow from all
+			</Directory>
+
+			ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
+			<Directory "/usr/lib/cgi-bin">
+				AllowOverride None
+				Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
+				Order allow,deny
+				Allow from all
+			</Directory>
+
+			ErrorLog /var/log/apache2/error.log
+
+			# Possible values include: debug, info, notice, warn, error, crit,
+			# alert, emerg.
+			LogLevel warn
+
+			CustomLog /var/log/apache2/access.log combined
+			ServerSignature On
+
+		    Alias /doc/ "/usr/share/doc/"
+		    <Directory "/usr/share/doc/">
+			Options Indexes MultiViews FollowSymLinks
+			AllowOverride None
+			Order deny,allow
+			Deny from all
+			Allow from 127.0.0.0/255.0.0.0 ::1/128
+		    </Directory>
+		</VirtualHost>
+		' | sudo tee -a  /etc/apache2/sites-available/000-default.conf  > /dev/null 
 
 		################# PHP ####################################################
 		sudo apt install php libapache2-mod-php php-mysql php-curl php-gd \
